f91c26be11a2c27e02f7beaff80fa47d227dee91
Sergey Tolmachev <tolsi.ru@gmail.com>
2019-03-28 11:01:41 +0300

    rewrite samples to use backend functions

--- sample/mark_image.py
+++ sample/mark_image.py
@@ -1,26 +1,10 @@
 import sys
 
 from serialize_image import serialize_image
-import requests
+from node import mark_image
 
-NODE = "http://127.0.0.1:6862"
 CONTRACT_ID = "H7XhErK1YzmY2i1EzNpfcSZv44vEFQGKLa89gUjsi15"
 SENDER = "3N2ALKEtTHj2WBCxrmnCgBrf1AoTuv84bbF"
 
-def mark_image(image_base64):
-    r = requests.post(NODE + '/contracts/execute', json={
-        "contractId": CONTRACT_ID,
-        "fee": 10000000,
-        "sender": SENDER,
-        "type": 104,
-        "version": 1,
-        "params":[
-            {"key": "cmd", "value": "MARK", "type": "string"},
-            {"key": "photo", "value": image_base64, "type": "binary"}
-        ]
-    }, headers={'content-type': 'application/json', 'X-API-Key': 'vostok'})
-    if r.status_code != 200:
-        raise ValueError("Bad request: " + r.text)
-
 if __name__ == '__main__':
-    mark_image(serialize_image(sys.argv[1]))
+    mark_image(CONTRACT_ID, SENDER, serialize_image(sys.argv[1]))
--- sample/register_all_faces.py
+++ sample/register_all_faces.py
@@ -1,29 +1,11 @@
 from serialize_image import serialize_image
 import os
-import requests
+from node import register_face
 
-NODE = "http://127.0.0.1:6862"
 CONTRACT_ID = "H7XhErK1YzmY2i1EzNpfcSZv44vEFQGKLa89gUjsi15"
 SENDER = "3N2ALKEtTHj2WBCxrmnCgBrf1AoTuv84bbF"
-
-def register_face(name, details, image_base64):
-    r = requests.post(NODE + '/contracts/execute', json={
-        "contractId": CONTRACT_ID,
-        "fee": 10000000,
-        "sender": SENDER,
-        "type": 104,
-        "version": 1,
-        "params":[
-            {"key": "cmd", "value": "ADD", "type": "string"},
-            {"key": "photo", "value": image_base64, "type": "binary"},
-            {"key": "name", "value": name, "type": "string"},
-            {"key": "details", "value": details, "type": "string"}
-         ]
-    }, headers={'content-type': 'application/json', 'X-API-Key': 'vostok'})
-    if r.status_code != 200:
-        raise ValueError("Bad request: " + r.text)
 
 faces_dir = "../faces"
 
 if __name__ == '__main__':
-    [register_face(file.split(".")[0], "Waves employee", serialize_image(os.path.join(faces_dir, file))) for file in os.listdir(faces_dir) if os.path.isfile(os.path.join(faces_dir, file))]
+    [register_face(CONTRACT_ID, SENDER, file.split(".")[0], "Waves employee", serialize_image(os.path.join(faces_dir, file))) for file in os.listdir(faces_dir) if os.path.isfile(os.path.join(faces_dir, file))]
